564c6e302db8496369e9bb26e086dea06e1b9638
zhoujie <zhoujie16m@163.com>
2018-12-28 01:21:20 +0800

    1

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,21 @@
 <template>
   <div id="app">
-    <div class="text-left">当前时间</div>
-    <div class="text-left">{{time_now.format('YYYY-MM-DD HH:mm:ss')}}</div>
+    <div class="text-left">本地时间 比 北京时间快约 {{time_wucha}} 毫秒</div>
     <div class="text-left">提交的时间</div>
-    <input v-model="timeSel" type="datetime-local">
+    <input v-model="timeSel" type="datetime-local" readonly="readonly">
     <div class="text-left">姓名</div>
     <input v-model="name" type="text" placeholder="姓名">
     <div class="text-left">手机</div>
     <input v-model="mobile" type="text" placeholder="手机号">
-    <button @tap="startHandleClick" type="button" class="mui-btn mui-btn-blue">开始</button>
+    <button ref="submitBtn" @tap="startHandleClick" type="button" class="mui-btn mui-btn-blue">开始</button>
     <div class="log-div text-left">
-      <div class="mui-scroll-wrapper">
-        <div class="mui-scroll">
-          <div v-for="item in logInfoArr">{{item}}</div>
-        </div>
-      </div>
+      <!-- <div class="mui-scroll-wrapper"> -->
+        <!-- <div class="mui-scroll"> -->
+				<div class="log-wrap" ref="logWrap">
+          <div :key="i" v-for="(item,i) in logInfoArr_res">{{item}}</div>
+				</div>
+        <!-- </div> -->
+      <!-- </div> -->
     </div>
   </div>
 </template>
@@ -28,13 +29,14 @@
     data() {
       return {
         moment,
-        name: '测试',
-        mobile: '111222333',
-        // timeSel: moment().format('YYYY-MM-DDTHH:mm'),
-        timeSel: moment().format('YYYY-MM-28T08:59:59'),
+        name: '周杰',
+        mobile: '17756202920',
+        timeSel: moment().format('YYYY-MM-DDT08:59:59'),
+        // timeSel: moment().format('YYYY-MM-28T00:58:00'),
         time_wucha: '',//本地时间和北京时间 快 毫秒
         time_now: moment(),//当前时间 动态变化 到点停止
         logInfoArr:[],
+				scrollView:null
       }
     },
     computed:{
@@ -44,33 +46,39 @@
     },
     mounted() {
       window._self = this
-      mui('.mui-scroll-wrapper').scroll()
-      this.logPrint(`页面加载完成, ${moment().format('YYYY-MM-DD HH:mm:ss')}`)
-
+			// this.scrollView = mui('.mui-scroll-wrapper').scroll()
+      this.logPrint(`${moment().format('YYYY-MM-DD HH:mm:ss')}  页面加载完成`)
+			this.computeTimeDiff()
     },
     methods: {
       startHandleClick() {
+				this.$refs.submitBtn.disabled = true
+				this.$refs.submitBtn.innerHTML = '正在运行...'
         let timer = setInterval(() => {
           this.time_now = moment()
           if (this.time_now.diff(moment(this.timeSel)) > 0) {
             //时间到了
-            this.logPrint(`时间到了, ${this.time_now.format('YYYY-MM-DD HH:mm:ss')}`)
+            this.logPrint(`${this.time_now.format('HH:mm:ss')}  时间到了`)
             this.updateStart()
             clearInterval(timer)
           } else {
-            this.logPrint(`当前时间: ${this.time_now.format('YYYY-MM-DD HH:mm:ss')}`)
+            this.logPrint(`${this.time_now.format('HH:mm:ss')}  正在等待...`)
           }
         }, 500)
       },
       //
       updateStart() {
-        this.logPrint('开始提交')
+        this.logPrint(`${moment().format('HH:mm:ss')}  开始提交`)
         let timer = setInterval(() => {
           this.formAction()
         }, 500)
         setTimeout(() => {
           clearInterval(timer)
-        }, 1000)
+					setTimeout(()=>{
+						this.logPrint(`${moment().format('HH:mm:ss')}  运行结束`)
+					},1000)
+					this.$refs.submitBtn.innerHTML='运行结束'
+        }, 5000)
       },
       computeTimeDiff() {
         //计算时间误差
@@ -78,6 +86,7 @@
           let time_bg = moment(date)
           let time_local = moment()
           this.time_wucha = time_local.diff(time_bg)
+					this.logPrint(`本地时间, ${time_local.format('YYYY-MM-DD HH:mm:ss')}`)
           this.logPrint(`北京时间, ${time_bg.format('YYYY-MM-DD HH:mm:ss')}`)
           this.logPrint(`本地时间比北京时间快 ${this.time_wucha} 毫秒`)
         })
@@ -92,12 +101,13 @@
         })
       },
       formAction() {
-        this.testUpdate(this.name, this.mobile)
+				this.xiangQiUpdate(this.name, this.mobile)
+        // this.testUpdate(this.name, this.mobile)
           .then(data => {
             if (data.r === 0) {
-              this.logPrint(`提交成功', ${moment().format('YYYY-MM-DD HH:mm:ss')}`)
+              this.logPrint(`${moment().format('HH:mm:ss')}  提交成功'`)
             } else {
-              this.logPrint(`提交失败', ${moment().format('YYYY-MM-DD HH:mm:ss')}, ${data.r}`)
+              this.logPrint(`${moment().format('HH:mm:ss')}  提交失败  ${data.r}`)
             }
           })
           .catch(() => {
@@ -106,6 +116,8 @@
         // this.xiangQiUpdate('哈哈', '16543253567')
       },
       testUpdate(name, mobile) {
+				this.logPrint(`${moment().format('YYYY-MM-DD HH:mm:ss')}`)
+				this.logPrint(`提交 姓名:${name} 电话:${mobile}`)
         return new Promise((resolve, reject) => {
           var url = 'http://ka7js75mkc9i2gnz.mikecrm.com/handler/web/form_runtime/handleSubmit.php'
           var params = {
@@ -140,6 +152,7 @@
         })
       },
       xiangQiUpdate(name, mobile) {
+				this.logPrint(`提交 姓名:${name} 电话:${mobile}`)
         return new Promise((resolve, reject) => {
           var url = 'http://gis1z4xshb2s37ki.mikecrm.com/handler/web/form_runtime/handleSubmit.php'
           var params = {
@@ -176,6 +189,9 @@
       },
       logPrint(msg){
         this.logInfoArr.unshift(msg)
+				// this.$nextTick(()=>{
+					// $(document).scrollTop(999999)
+				// })
       }
     },
   }
@@ -201,7 +217,12 @@
       position: relative;
       height: 200px;
       background-color: white;
+			overflow: scroll;
     }
+		.log-wrap > div{
+			height: 30px;
+			line-height: 30px;
+		}
   }
 
 </style>
