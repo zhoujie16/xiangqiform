b0e4f18ff3d2bcf59620b79dd1040471b4234917
zhoujie <zhoujie16m@163.com>
2019-01-15 17:55:46 +0800

    1

--- src/App3.vue
+++ src/App3.vue
@@ -1,6 +1,16 @@
 <template>
   <div id="app">
+    <div class="form">
+      <div ref="formTips" class="form-tips">
+        <div class="xq-title">享骑表单自动提交</div>
+        <div class="xq-time-tips">本地时间 比 北京时间快约 {{time_wucha}} 毫秒</div>
+      </div>
+    </div>
     <div class="console-div" style="padding: 10px;">
+      <div>{{time_now.format('YYYY-MM-DD HH:mm:ss')}}</div>
+      <div>距离{{timeSel.format('MM月DD日')}}9点整</div>
+      <div>{{parseInt(-countdown/1000)}}秒</div>
+      <div>数据将在9点整连续提交多次</div>
       <div class="log-wrap">
         <div :key="i" v-for="(item,i) in logInfoArr_res">{{item}}</div>
       </div>
@@ -14,9 +24,16 @@
   const ENVIRONMENT = 'XQ'
   //麦客网地址
   // const MK_FORM_URL = 'http://gis1z4xshb2s37ki.mikecrm.com/JI5p0O4'//xq的
+  // const MK_FORM_URL = 'http://gis1z4xshb2s37ki.mikecrm.com/dcSGLtc'//失效的
+  const MK_FORM_URL = 'http://ka7js75mkc9i2gnz.mikecrm.com/OaFtcNB'//测试的
+  // const MK_FORM_URL = 'http://ka7js75mkc9i2gnz.mikecrm.com/wsEqMGV'//不在时间范围
   /////////////
   const TEST_URL = 'http://ka7js75mkc9i2gnz.mikecrm.com/handler/web/form_runtime/handleSubmit.php'
+  //test: {"cvs":{"i":200103402,"t":"OaFtcNB","s":200238984,"acc":"TIXaEDgbjvYpGW0lLaGnmHpnavB798sm","r":"","c":{"cp":{"200992865":"11111","200992866":"11111"}}}}
+  // const XQ_URL = 'http://gis1z4xshb2s37ki.mikecrm.com/handler/web/form_runtime/handleSubmit.php'
   const XQ_URL = 'http://gis1z4xshb2s37ki.mikecrm.com/handler/web/form_runtime/handleSubmit.php'
+  //12-27: {"cvs":{"i":200241815,"t":"dcSGLtc","s":200710795,"acc":"YGtEHjWUXSBV2nlMggbzeklRaAB5MCGD","r":"","c":{"cp":{"202474905":name,"202474906":mobile},"ext":{}}}}
+  //12-28: {"cvs":{"i":200243921,"t":"JI5p0O4","s":200712652,"acc":"1ulXDVmPcXkgiujwkJHY0Ijdrcv5K3q4","r":"","c":{"cp":{"202500664":"周杰","202500665":"17756202920"},"ext":{}}}}
   //
   window.moment = moment
   export default {
@@ -25,8 +42,17 @@
     data() {
       return {
         moment,
+        name: '周杰',
+        mobile: '17756202920',
+        timeSel: moment(moment().format('YYYY-MM-DDT08:59:59')),
+        // timeSel: moment(moment().format('YYYY-MM-DDT00:00:00')),
+        time_wucha: '',//本地时间和北京时间 快 毫秒
         time_now: moment(),//当前时间 动态变化 到点停止
         logInfoArr: [],
+        scrollView: null,
+        countdown: 0,//倒计时
+        btnDisabled: true,
+        timer_update: {}
       }
     },
     computed: {
@@ -38,7 +64,6 @@
       window._self = this
       this.logPrint(`${moment().format('HH:mm:ss')}  页面加载完成`)
       this.computeTimeDiff()
-      return
       var timer = setInterval(() => {
         console.log(timer)
         this.time_now = moment()
@@ -70,32 +95,42 @@
       },
       computeTimeDiff() {
         //计算时间误差
-        getBJTime().then(date => {
+        this.getBJTime().then(date => {
           let time_bg = moment(date)
           let time_local = moment()
-          let time_wucha = time_local.diff(time_bg)
+          this.time_wucha = time_local.diff(time_bg)
           this.logPrint(`本地时间, ${time_local.format('YYYY-MM-DD HH:mm:ss')}`)
           this.logPrint(`北京时间, ${time_bg.format('YYYY-MM-DD HH:mm:ss')}`)
-          this.logPrint(`本地时间比北京时间快 ${time_wucha} 毫秒`)
-          // this.testMKForm()
+          this.logPrint(`本地时间比北京时间快 ${this.time_wucha} 毫秒`)
+          this.testMKForm()
+        }).catch(err => {
+          this.$createDialog({
+            type: 'alert',
+            title: '提示',
+            content: err,
+          }).show()
+          // const tips = `
+          // 当前浏览器不支持跨域，请在PC端使用chrome内核的浏览器，安装跨域插件后使用。
+          // 下载地址：<a href="http://chromecj.com/web-development/2018-07/1481.html">谷歌跨域扩展插件:Allow-Control-Allow-Origin<a>
+          // `
+          // $(this.$refs.formTips).html(tips)
         })
-
-        function getBJTime() {
-          return new Promise((resolve, reject) => {
-            $.ajax({
-              url: 'http://api.m.taobao.com/rest/api3.do?api=mtop.common.getTimestamp',
-              type: 'get',
-              dataType: 'json',
-              success(data) {
-                const date = data.data.t
-                resolve(Number(date))
-              },
-              error() {
-                reject('当前浏览器不支持跨域，请在PC端使用chrome内核的浏览器，安装跨域插件后使用')
-              }
-            })
+      },
+      getBJTime() {
+        return new Promise((resolve, reject) => {
+          $.ajax({
+            url: 'http://api.m.taobao.com/rest/api3.do?api=mtop.common.getTimestamp',
+            type: 'get',
+            dataType: 'json',
+            success(data) {
+              const date = data.data.t
+              resolve(Number(date))
+            },
+            error() {
+              reject('当前浏览器不支持跨域，请在PC端使用chrome内核的浏览器，安装跨域插件后使用')
+            }
           })
-        }
+        })
       },
       formAction() {
         this.testUpdate('周杰', '17756202920')
@@ -181,17 +216,34 @@
           .then(data => {
             if (data === '') {
               this.logPrint(`${moment().format('HH:mm:ss')} 你的IP可能被限制访问`)
+              this.$createDialog({
+                type: 'alert',
+                title: '提示',
+                content: '你的IP可能被限制访问',
+                icon: 'cubeic-alert'
+              }).show()
               return
             }
             if (data.indexOf('SOUL') === -1) {
               this.logPrint(`${moment().format('HH:mm:ss')} 你要提交的表单已经失效`)
+              this.$createDialog({
+                type: 'alert',
+                title: '提示',
+                content: '你要提交的表单已经失效',
+                icon: 'cubeic-alert'
+              }).show()
               return
             }
             this.logPrint(`${moment().format('HH:mm:ss')} 检测表单数据正常`)
             this.btnDisabled = false
           })
           .catch(err => {
-
+            this.$createDialog({
+              type: 'alert',
+              title: '提示',
+              content: err,
+              icon: 'cubeic-alert'
+            }).show()
           })
 
         function ajaxAction() {
@@ -228,6 +280,43 @@
     height: 100vh;
     text-align: initial;
     overflow: hidden;
+
+
+    .form {
+      padding: 10px;
+
+      .xq-title {
+        font-size: 25px;
+        text-align: center;
+        line-height: 25px;
+        margin: 10px 0;
+      }
+
+      .xq-time-tips {
+        font-size: 14px;
+        padding: 2px 0;
+        text-align: center;
+        color: gray;
+      }
+
+      .xq-tips {
+        font-size: 14px;
+        padding: 2px 0;
+        text-align: center;
+        color: gray;
+      }
+
+      .label {
+        height: 40px;
+        line-height: 40px;
+        margin-top: 15px;
+      }
+
+      .btn-wrap {
+        padding: 10px 0px;
+
+      }
+    }
   }
 
 
--- src/main.js
+++ src/main.js
@@ -1,11 +1,9 @@
 import Vue from 'vue'
 import './cube-ui'
 import App from './App.vue'
-import Cube from 'cube-ui'
 
 Vue.config.productionTip = false
 
-Vue.use(Cube)
 new Vue({
   render: h => h(App),
 }).$mount('#app')
